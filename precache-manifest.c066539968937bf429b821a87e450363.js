95b1dd3496a00df2e3fbdb8f522c5daa55a75a6c
Mitchell Kerr <mkerr@vailsys.com>
2020-01-23 02:20:31 -0600

    Updates

---
+++ precache-manifest.c066539968937bf429b821a87e450363.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75c3ae862d353a5dfcdcfbc2cfd0019a",
+    "url": "/kickback-site/index.html"
+  },
+  {
+    "revision": "339515ecca1959a60fcc",
+    "url": "/kickback-site/static/css/2.aa46e993.chunk.css"
+  },
+  {
+    "revision": "6eb34cb9d95284890984",
+    "url": "/kickback-site/static/css/main.9a80a015.chunk.css"
+  },
+  {
+    "revision": "339515ecca1959a60fcc",
+    "url": "/kickback-site/static/js/2.d6a168d6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/kickback-site/static/js/2.d6a168d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6eb34cb9d95284890984",
+    "url": "/kickback-site/static/js/main.4416e807.chunk.js"
+  },
+  {
+    "revision": "41528a80c7c976cc022f",
+    "url": "/kickback-site/static/js/runtime-main.93406b4d.js"
+  },
+  {
+    "revision": "9ee42c0b1fced6100c87213e3318231e",
+    "url": "/kickback-site/static/media/sam-train-pic.9ee42c0b.jpg"
+  }
+]);
